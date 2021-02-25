84fa2b1b2f8b4bf27d23edd5149d98a767a4eb7e
RUnroe <ryanunroe@gmail.com>
2021-02-25 01:46:01 -0700

    Add getters and setters to poker model

--- src/poker/models/PokerModel.java
+++ src/poker/models/PokerModel.java
@@ -6,11 +6,93 @@ import models.Player;
 import java.util.ArrayList;
 
 public class PokerModel {
+
+
     private ArrayList<Player> playerList = new ArrayList<>();
     private ArrayList<Player> playersWhoHaveNotFolded = new ArrayList<>();
     private Deck deck = new Deck(0); //I have no clue why the deck class doesn't have a default constructor...
     private int moneyPool = 0;
     private int currentPlayerIndex = 0;
     private int currentBet;
+
+
+
+    //Getters and Setters
+    public ArrayList<Player> getPlayerList() {
+        return playerList;
+    }
+
+    public void setPlayerList(ArrayList<Player> playerList) {
+        this.playerList = playerList;
+    }
+    public void addPlayerToPlayerList(Player player) {
+        this.playerList.add(player);
+    }
+
+    public ArrayList<Player> getPlayersWhoHaveNotFolded() {
+        return this.playersWhoHaveNotFolded;
+    }
+
+    public void resetPlayersWhoHaveNotFolded() {
+        this.playersWhoHaveNotFolded = (ArrayList<Player>) this.playerList.clone();
+    }
+
+    public void foldPlayerByIndex(int indexOfPlayer) {
+        this.playersWhoHaveNotFolded.remove(indexOfPlayer);
+    }
+
+    public Deck getDeck() {
+        return this.deck;
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+
+    public int getMoneyPool() {
+        return this.moneyPool;
+    }
+
+    public void setMoneyPool(int moneyPool) {
+        this.moneyPool = moneyPool;
+    }
+    public void addToMoneyPool(int amountToAdd) {
+        this.moneyPool += amountToAdd;
+    }
+
+
+    public int getCurrentPlayerIndex() {
+        return this.currentPlayerIndex;
+    }
+
+    public void setCurrentPlayerIndex(int currentPlayerIndex) {
+        this.currentPlayerIndex = currentPlayerIndex;
+    }
+
+    //By default, switch by 1. We do not want to increment the playerIndex when a player folds(is removed from arraylist)
+    public void switchTurn() {
+        switchTurn(1);
+    }
+    public void switchTurn(int incrementValue) {
+        this.currentPlayerIndex += incrementValue;
+        //If next player index is out of bounds, wrap back to player 0
+        this.currentPlayerIndex %= getNumberOfActivePlayers();
+    }
+
+    public int getCurrentBet() {
+        return currentBet;
+    }
+
+    public void setCurrentBet(int currentBet) {
+        this.currentBet = currentBet;
+    }
+
+    public void raiseCurrentBet(int amount) {
+        this.currentBet += amount;
+    }
+
+    public int getNumberOfActivePlayers() {
+        return this.playersWhoHaveNotFolded.size();
+    }
 
 }
