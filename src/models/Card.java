2676b8b05cb3153333883d56e71915d69c3b2ce1
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2021-02-25 05:03:27 -0700

    added methods for loading in image views in card class; renamed all cards to fit project formatting

--- src/models/Card.java
+++ src/models/Card.java
@@ -1,17 +1,25 @@
 package models;
 
-import javax.swing.text.html.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 
 public class Card {
     private final String name;
     private final ESuit suit;
     private final ERank rank;
-    private ImageView imageView;
+    private final ImageView imageView;
+    private boolean isFlipped;
 
     public Card(ESuit suit, ERank rank) {
         this.suit = suit;
         this.rank = rank;
         this.name = getRankAsString() + " of " + getSuitAsString();
+        this.imageView = loadImageViewFromPath("src/resources/images/cards/PNG/black/" + getSuitAsString() + "_" + getRankAsString() + "_black.png");
+        this.isFlipped = false;
     }
 
     public ESuit getSuit() {
@@ -73,5 +81,44 @@ public class Card {
             case HEARTS -> "Hearts";
             case SPADES -> "Spades";
         };
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    private Image loadImageFromPath(String path) {
+        Image image = null;
+        try {
+            FileInputStream fileInputStream = new FileInputStream(path); // opens an input stream on the path
+            image = new Image(fileInputStream); // loads an image from the input stream
+        } catch (IOException e) {
+//                e.printStackTrace();
+            System.err.println("Image could not be loaded: File not found at " + path);
+        }
+        return image;
+    }
+
+    public ImageView loadImageViewFromPath(String path) {
+        Image image = loadImageFromPath(path);
+        ImageView imageView = null;
+        if (image != null) {
+            imageView = new ImageView(image); // creates an image view from loaded image
+        } else {
+            System.err.println("ImageView could not be loaded: source Image was null");
+        }
+        return imageView;
+    }
+
+    public boolean isFlipped() {
+        return isFlipped;
+    }
+
+    public void setFlipped(boolean flipped) {
+        isFlipped = flipped;
+    }
+
+    public void flip() {
+        setFlipped(!isFlipped());
     }
 }
